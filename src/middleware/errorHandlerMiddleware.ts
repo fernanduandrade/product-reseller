5098ec8d06a52861fc926f0a703b2d7989322c54
fernanduandrade <onanduandrade@gmail.com>
2023-08-06 14:27:48 -0300

    fix: status code quando o serviço não estiver disponível

--- src/middleware/errorHandlerMiddleware.ts
+++ src/middleware/errorHandlerMiddleware.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from 'express'
 
 export const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => { // responding to client
-  return res.status(500)
+  return res.status(503)
                 .json({
                   error: true,
                   message: process.env.ENVIRONMENT === 'DEVELOPMENT'
                     ? error.message
-                    : 'Service unavaliable'
+                    : 'Service unavaliable, contact the suport team.'
                 }) 
 }
 
